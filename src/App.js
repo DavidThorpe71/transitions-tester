d68f6f67b59add4aa3459d8c5c93ff56d72a9bf3
David Thorpe <davidthorpe878@gmail.com>
2018-08-21 19:53:51 +0100

    begin creat a portal

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
+import Toggle from './components/ToggleRPC'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="test">test</p>
+        <Toggle>
+          {({ on, toggle }) => (
+            <Fragment>
+              {on && <h1>Show mme</h1>}
+              <button onClick={toggle}>Show / Hide</button>
+            </Fragment>
+          )}
+        </Toggle>
+
       </div>
     );
   }
