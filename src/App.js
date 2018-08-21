67bfd3170ee8f3dd8d0a67d6d11e9c45758402d4
David Thorpe <davidthorpe878@gmail.com>
2018-08-21 20:13:02 +0100

    added a modal

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React, { Component, Fragment } from 'react';
 import Toggle from './components/ToggleRPC'
 import './App.css';
+import Portal from './components/Portal';
+import Modal from './components/Modal';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <p className="test">test</p>
+
         <Toggle>
           {({ on, toggle }) => (
             <Fragment>
-              {on && <h1>Show mme</h1>}
-              <button onClick={toggle}>Show / Hide</button>
+              <button onClick={toggle}>Login</button>
+              <Modal on={on} toggle={toggle}>
+                <h1>Still in modal</h1>
+              </Modal>
             </Fragment>
           )}
         </Toggle>
-
       </div>
     );
   }
