3059d1b7419b52bdb9cf6406b3226528b1c80188
David Thorpe <davidthorpe878@gmail.com>
2018-08-21 20:26:36 +0100

    modal styles added

--- src/App.js
+++ src/App.js
@@ -8,14 +8,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1>MASSIVE HEADER</h1>
         <p className="test">test</p>
-
         <Toggle>
           {({ on, toggle }) => (
             <Fragment>
               <button onClick={toggle}>Login</button>
               <Modal on={on} toggle={toggle}>
-                <h1>Still in modal</h1>
+                <h1>Still</h1>
               </Modal>
             </Fragment>
           )}
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -9,10 +9,14 @@ class Modal extends Component {
       <Portal>
         {on &&
           <ModalWrapper>
-            <button onClick={toggle}>Close</button>
-            <div>
-              {children}
-            </div>
+            <ModalCard>
+              <CloseButton onClick={toggle}>Close</CloseButton>
+              <div>
+                {children}
+              </div>
+
+            </ModalCard>
+            <Background onClick={toggle} />
           </ModalWrapper>
         }
       </Portal>
@@ -26,6 +30,36 @@ const ModalWrapper = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: teal;
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ModalCard = styled.div`
+  position: relative;
+  background-color: white;
+  border-radius: 3px;
+  padding: 15px;
+  box-shadow: 2px 2px 10px rgba(0,0,0,0.3);
+  z-index: 10;
+  min-width: 320px;
+  margin-bottom: 400px;
+`;
+
+const CloseButton = styled.button`
+  position: absolute;
+  top: 0;
+  right: 0;
+`;
+
+const Background = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: black;
+  opacity: 0.5;
+`;
+
 export default Modal;
